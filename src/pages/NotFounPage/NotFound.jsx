453b79017f7acc26bfad51d803354f2ea4e513e4
Ivan Radko <mr.radkoivan@gmail.com>
2023-07-17 13:30:44 +0300

    fix not found page style for mobile

--- src/pages/NotFounPage/NotFound.jsx
+++ src/pages/NotFounPage/NotFound.jsx
@@ -1,17 +1,12 @@
-import { Container } from 'components/StyledContainer';
 import notFound from '../../image/NotFoundPageBackground.png';
+import { StyledNotFoundContainer } from './StyledNotFound';
 
 export default function NotFound() {
   return (
-    <div
-      style={{
-        backgroundColor: '#FFDE59',
-        height: '100vh',
-        width: '100vw',
-        margin: '50px auto',
-      }}
-    >
-      <img src={notFound} alt="not found" width={'100%'} />
-    </div>
+    <StyledNotFoundContainer>
+      <div>
+        <img src={notFound} alt="not found" width={'80%'} />
+      </div>
+    </StyledNotFoundContainer>
   );
 }
---
+++ src/pages/NotFounPage/StyledNotFound.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledNotFoundContainer = styled.div`
+  background-color: #ffde59;
+  height: 100vh;
+  width: 100vw;
+  margin-top: 50px;
+
+  div {
+    display: flex;
+    justify-content: center;
+  }
+`;
