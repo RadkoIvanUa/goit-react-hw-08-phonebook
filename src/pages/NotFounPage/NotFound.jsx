39f6810d4a442e9f04722f35d0582c2da1ff2a29
Ivan Radko <mr.radkoivan@gmail.com>
2023-07-17 13:12:59 +0300

    fix some style

--- src/pages/NotFounPage/NotFound.jsx
+++ src/pages/NotFounPage/NotFound.jsx
@@ -3,10 +3,15 @@ import notFound from '../../image/NotFoundPageBackground.png';
 
 export default function NotFound() {
   return (
-    <div style={{ backgroundColor: '#FFDE59', height: '100vh' }}>
-      <Container style={{ margin: '50px auto' }}>
-        <img src={notFound} alt="not found" width={'100%'} />
-      </Container>
+    <div
+      style={{
+        backgroundColor: '#FFDE59',
+        height: '100vh',
+        width: '100vw',
+        margin: '50px auto',
+      }}
+    >
+      <img src={notFound} alt="not found" width={'100%'} />
     </div>
   );
 }
